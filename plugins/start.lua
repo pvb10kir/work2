06758fc7d5582ec9f3bcdb22c16f05c9804438f5
pvb10kir <sehr73@chmail.ir>
2018-01-05 10:50:27 +0330

    Update start.lua

--- plugins/start.lua
+++ plugins/start.lua
@@ -75,7 +75,7 @@ local function do_keyboard_act()
 end
 local action = function(msg, blocks, ln)
 	if blocks[1] == 'start' or blocks[1] == 'help' then
-	db:hset('bot:users', msg.from.id)
+	db:hset('bot:users', msg.from.id, 'xx')
 	db:hincrby('bot:gen', 'users', 1)
         if msg.chat.type == 'private' then
             local message = [[توضیحات برنامه صیغه یاب❤️👌🏻👇🏻]]
